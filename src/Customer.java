d3251c502a50f9f241dd67d6c2e7625d4796b36c
Samer Ismael <samerismaeelbat2@hotmail.com>
2023-04-27 15:58:11 +0200

    Creating classes

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,17 @@
-public class Customer {
+public class Customer extends Person {
+
+    private String customerNumber;
+
+    public Customer(String name, String address, int age, String customerNumber) {
+        super(name, address, age);
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,2 +1,18 @@
-public class Worker {
+public class Worker extends Person {
+
+    private String employeeNumber;
+
+    public Worker(String name, String address, int age, String employeeNumber) {
+        super(name, address, age);
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
 }
